c487229cd32f5737bfa57327465ea1398cad98c1
Ryan Zegray <ryan.zegray@gmail.com>
2020-01-23 20:48:29 -0500

    Add eslint
    extract
    
    update
    
    more update
    
    move logic to util
    
    todo
    
    Signed-off-by: Ryan Zegray <ryan.zegray@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,15 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import * as starter from './util/starter';
-import { reverse } from 'dns';
+import * as vscode from "vscode";
+import * as starter from "./util/starter";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "microprofilestarter" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-
+export function activate(context: vscode.ExtensionContext): void {
 	context.subscriptions.push(
-		vscode.commands.registerCommand('extension.microProfileStarter', () => starter.generateProject())
+		vscode.commands.registerCommand("extension.microProfileStarter", () => starter.generateProject())
 	);
 }
 
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export function deactivate(): void { }
--- src/util/util.ts
+++ src/util/util.ts
@@ -1,75 +1,156 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 import {
-    OpenDialogOptions,
-    Uri,
-    window
+	OpenDialogOptions,
+	Uri,
+	window
 } from "vscode";
-
-let properties = require('../properties');
-
-export async function openDialogForFolder(customOptions: OpenDialogOptions): Promise<Uri | undefined> {
-    const options: OpenDialogOptions = {
-        canSelectFiles: false,
-        canSelectFolders: true,
-        canSelectMany: false,
-    };
-    const result = await window.showOpenDialog(Object.assign(options, customOptions));
-    if (result && result.length) {
-        return Promise.resolve(result[0]);
-    } else {
-        return Promise.resolve(undefined);
-    }
-}
-
-export async function mapToPropertiesFile(arrayName : string, originalArray : string[]) {
-	var mappedArray : string[] = new Array();
-	mappedArray =  originalArray.map(function (elem) {
-		return properties[arrayName][elem];
-	});
-	mappedArray.sort().reverse();
-	return mappedArray;
-}
-
-function capitalizeFirstLetter(newString :string) {
-    return newString.charAt(0).toUpperCase() + newString.slice(1);
-}
-
-export async function mapToDescription(prevArray : string[], descriptions: Object) {
-	var specItems : vscode.QuickPickItem[] = new Array();
-	prevArray.forEach(element => {
-		var content = descriptions[element as keyof typeof descriptions];
-		var contentStr = new String(content);
-
-		var splits = contentStr.split(" - ");
-		specItems.push({
-			'label': splits[0],
-			// 'detail': capitalizeFirstLetter(splits[1])
-			'detail': splits[1]
-		});
-	});
-	return specItems;
-}
-
-export function getKeyFromValue(value: Object, arr : any) {
-	return Object.keys(arr).find(key => arr[key] === value);
-}
+import { MP_SERVER_LABELS, MP_VERSION_LABELS } from "../properties";
 
 
-export async function resolveSpecs(specifications : String[], descriptions: Object){
-	var dataString: String = "";
-	if (specifications !== undefined && specifications.length > 0) {
-		dataString += ', "selectedSpecs":[';
-		specifications.forEach(async element => {
-			var apiSpec;
-			if (element === specifications[0]){
-				apiSpec = getKeyFromValue(element, descriptions);
-				dataString = dataString + '"' + apiSpec + '"';
-			} else {
-				apiSpec = getKeyFromValue(element, descriptions);
-				dataString += ', "' + apiSpec + '"';
+export async function askForGroupID(): Promise<string | undefined> {
+	return await vscode.window.showInputBox({
+		placeHolder: "e.g. com.example",
+		prompt: "Specify a Group Id for your project.",
+		value: "com.example",
+		ignoreFocusOut: true,
+		validateInput: (value: string) => {
+			if (value.trim().length === 0) {
+				return "Group Id is required";
 			}
-		});
-		dataString = dataString + ']';
+			if (value.indexOf(" ") >= 0) {
+				return "Group Id cannot contain a blank space";
+			}
+			return null;
+		},
+	});
+}
+
+export async function askForArtifactID(): Promise<string | undefined> {
+	return await vscode.window.showInputBox({
+		placeHolder: "demo",
+		prompt: "Specify an Artifact Id for your project.",
+		value: "demo",
+		ignoreFocusOut: true,
+		validateInput: (value: string) => {
+			if (value.trim().length === 0) {
+				return "Artifact Id is required";
+			}
+			if (value.indexOf(" ") >= 0) {
+				return "Artifact Id cannot contain a blank space";
+			}
+			return null;
+		},
+	});
+}
+
+
+export async function askForJavaSEVersion(): Promise<string | undefined> {
+	const SUPPORTED_JAVA_SE_VERSIONS = ["SE8"];
+
+	return await vscode.window.showQuickPick(
+		SUPPORTED_JAVA_SE_VERSIONS,
+		{ ignoreFocusOut: true, placeHolder: "Select a Java SE version." },
+	);
+}
+
+export async function askForMPVersion(mpVersions: string[]): Promise<string | undefined> {
+	interface MPVersionOption extends vscode.QuickPickItem {
+		label: string; // label is the long-name that is displaed in vscode
+		version: string; // version is the shortname that is used internally by the microprofile starter api
 	}
-	return dataString;
+
+	const mpVersionOptions: MPVersionOption[] = [];
+	for (const mpVersion of mpVersions) {
+		if (MP_VERSION_LABELS[mpVersion] != null) {
+			mpVersionOptions.push({
+				label: MP_VERSION_LABELS[mpVersion],
+				version: mpVersion
+			});
+		}
+	}
+
+	const mpVersionQuickPickResult = await vscode.window.showQuickPick(
+		mpVersionOptions,
+		{ ignoreFocusOut: true, placeHolder: "Select a MicroProfile version." },
+	);
+
+	if (mpVersionQuickPickResult != null) {
+		return mpVersionQuickPickResult.version;
+	}
+
+	return undefined;
+}
+
+export async function askForMPserver(mpServers: string[]): Promise<string | undefined> {
+	interface MPServerOption extends vscode.QuickPickItem {
+		label: string; // label is the long-name that is displaed in vscode
+		server: string; // server is the shortname that is used internally by the microprofile starter api
+	}
+
+	const mpServerOptions: MPServerOption[] = [];
+	for (const mpServer of mpServers) {
+		if (MP_SERVER_LABELS[mpServer] != null) {
+			mpServerOptions.push({
+				label: MP_SERVER_LABELS[mpServer],
+				server: mpServer
+			});
+		}
+	}
+
+	const mpVersionQuickPickResult = await vscode.window.showQuickPick(
+		mpServerOptions,
+		{ ignoreFocusOut: true, placeHolder: "Select a MicroProfile server." },
+	);
+
+	if (mpVersionQuickPickResult != null) {
+		return mpVersionQuickPickResult.server;
+	}
+	return undefined;
+}
+
+export async function askForMPSpecifications(specs: string[], specDescriptions: Record<string, string>): Promise<Array<string> | undefined> {
+	interface MPSpecOption extends vscode.QuickPickItem {
+		spec: string;
+		label: string;
+		detail: string;
+	}
+
+	const mpSpecOptions: MPSpecOption[] = specs.map(spec => {
+		const fullDescriptionString = specDescriptions[spec];
+		const [name, desc] = fullDescriptionString.split("-");
+		return {
+			spec: spec,
+			label: name,
+			detail: desc,
+		};
+	});
+
+	const specResults: MPSpecOption[] | undefined = await vscode.window.showQuickPick(
+		mpSpecOptions,
+		{ ignoreFocusOut: true, canPickMany: true, placeHolder: "Select MicroProfile specifications." },
+	);
+
+	if (specResults != null) {
+		return specResults.map(result => result.spec);
+	}
+	return undefined;
+}
+
+export async function askForFolder(customOptions: OpenDialogOptions): Promise<Uri | undefined> {
+	const options: OpenDialogOptions = {
+		canSelectFiles: false,
+		canSelectFolders: true,
+		canSelectMany: false,
+	};
+	const result = await window.showOpenDialog(Object.assign(options, customOptions));
+
+	if (result && result.length > 0) {
+		return result[0];
+	}
+
+	return undefined;
+}
+
+export function capitalizeFirstLetter(newString: string): string {
+	return newString.charAt(0).toUpperCase() + newString.slice(1);
 }
