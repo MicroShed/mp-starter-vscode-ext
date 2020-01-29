464de130434f6ee4eed995838ab6b40952c2d060
Ryan Zegray <ryan.zegray@gmail.com>
2020-01-29 14:44:52 -0500

    travis
    Signed-off-by: Ryan Zegray <ryan.zegray@gmail.com>

---
+++ src/test/suite/extension.test.ts
@@ -0,0 +1,26 @@
+import { expect } from "chai";
+
+const MP_STARTER_IDENTIFIER = "MicroProfile-Community.mp-starter-vscode-ext";
+const MP_STARTER_COMMAND = "extension.microProfileStarter";
+
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+import * as vscode from "vscode";
+// import * as myExtension from '../extension';
+
+describe("VS Code extension", () => {
+  it("should be present", () => {
+    expect(vscode.extensions.getExtension(MP_STARTER_IDENTIFIER)).to.be.ok;
+  });
+
+  it("should register an activation event", () => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const extensionPackageJSON = vscode.extensions.getExtension(MP_STARTER_IDENTIFIER)!.packageJSON;
+
+    expect(extensionPackageJSON.activationEvents).to.include(
+      `onCommand:${MP_STARTER_COMMAND}`,
+      `The ${MP_STARTER_COMMAND} command is not registered as an activation event in package.json`
+    );
+    console.log(extensionPackageJSON);
+  });
+});
