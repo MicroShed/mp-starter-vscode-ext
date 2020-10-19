f555debeb9dc5fc4e6d8da1d2761b831036d5b04
Jana Manoharan <jana.manoharan@ibm.com>
2020-10-19 17:14:32 -0400

    fixed more validation errors
    Signed-off-by: Jana Manoharan <jana.manoharan@ibm.com>

--- webpack.config.js
+++ webpack.config.js
@@ -8,17 +8,17 @@ const config = {
   devtool: "source-map",
   entry: "./src/extension.ts",
   output: {
-      // the bundle is stored in the "dist" folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
-      path: path.resolve(__dirname, "out"),
-      filename: "extension.js",
-      libraryTarget: "commonjs2",
-      devtoolModuleFilenameTemplate: "../[resource-path]"
+    // the bundle is stored in the "dist" folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+    path: path.resolve(__dirname, "out"),
+    filename: "extension.js",
+    libraryTarget: "commonjs2",
+    devtoolModuleFilenameTemplate: "../[resource-path]",
   },
   externals: {
-    vscode: "commonjs vscode"
+    vscode: "commonjs vscode",
   },
   resolve: {
-    extensions: [".ts", ".js"]
+    extensions: [".ts", ".js"],
   },
   module: {
     rules: [
@@ -27,11 +27,11 @@ const config = {
         exclude: /node_modules/,
         use: [
           {
-            loader: "ts-loader"
-          }
-        ]
-      }
-    ]
-  }
+            loader: "ts-loader",
+          },
+        ],
+      },
+    ],
+  },
 };
 module.exports = config;
