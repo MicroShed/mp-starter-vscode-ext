9e85ba55c8d66b9227464cfe36ca005e4dd6ccf5
Jana Manoharan <jana.manoharan@ibm.com>
2020-10-19 14:10:34 -0400

    this resolves #65
    Signed-off-by: janamano <43358234+janamano@users.noreply.github.com>

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+// @ts-check
+
+"use strict";
+
+const path = require("path");
+const config = {
+    target: "node",
+    devtool: 'source-map',
+    entry: './src/extension.ts',
+    output: {
+        // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+        path: path.resolve(__dirname, 'out'),
+        filename: 'extension.js',
+        libraryTarget: 'commonjs2',
+        devtoolModuleFilenameTemplate: '../[resource-path]'
+    },
+    externals: {
+        vscode: 'commonjs vscode'
+      },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+          {
+            test: /\.ts$/,
+            exclude: /node_modules/,
+            use: [
+              {
+                loader: 'ts-loader'
+              }
+            ]
+          }
+        ]
+    }
+};
+module.exports = config;
