f7c993fd631da7492e764a2e630ae99b0aca2593
Jana Manoharan <jana.manoharan@ibm.com>
2020-10-19 17:04:31 -0400

    fixed validation errors
    Signed-off-by: Jana Manoharan <jana.manoharan@ibm.com>

--- webpack.config.js
+++ webpack.config.js
@@ -4,34 +4,34 @@
 
 const path = require("path");
 const config = {
-    target: "node",
-    devtool: 'source-map',
-    entry: './src/extension.ts',
-    output: {
-        // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
-        path: path.resolve(__dirname, 'out'),
-        filename: 'extension.js',
-        libraryTarget: 'commonjs2',
-        devtoolModuleFilenameTemplate: '../[resource-path]'
-    },
-    externals: {
-        vscode: 'commonjs vscode'
-      },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    module: {
-        rules: [
+  target: "node",
+  devtool: "source-map",
+  entry: "./src/extension.ts",
+  output: {
+      // the bundle is stored in the "dist" folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+      path: path.resolve(__dirname, "out"),
+      filename: "extension.js",
+      libraryTarget: "commonjs2",
+      devtoolModuleFilenameTemplate: "../[resource-path]"
+  },
+  externals: {
+    vscode: "commonjs vscode"
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
           {
-            test: /\.ts$/,
-            exclude: /node_modules/,
-            use: [
-              {
-                loader: 'ts-loader'
-              }
-            ]
+            loader: "ts-loader"
           }
         ]
-    }
+      }
+    ]
+  }
 };
 module.exports = config;
